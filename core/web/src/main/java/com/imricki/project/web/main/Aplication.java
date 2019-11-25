c76d1ef072ca3adf62f4fa49ff23d0698ebaf8a5
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2019-11-25 17:52:13 +0000

    data layer up

--- core/web/src/main/java/com/imricki/project/web/main/Aplication.java
+++ core/web/src/main/java/com/imricki/project/web/main/Aplication.java
@@ -2,10 +2,12 @@ package com.imricki.project.web.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = { "com" })
-@EnableJpaRepositories("com")
+@EnableJpaRepositories("com.*")
+@EntityScan("com.*")
+@SpringBootApplication(scanBasePackages = "com.*")
 public class Aplication {
 
 	public static void main(String[] args) {
